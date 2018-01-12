37c6d128832d00b88b1e8c04963768561f5c8446
David Espinoza <davidespinoza@att.net>
2018-01-12 01:01:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = "bob";
 
-function upperCaseCustomerName(aCustName){
-  return aCustName.toUpperCase();
+function upperCaseCustomerName(customerName){
+  return customerName.toUpperCase();
 }
